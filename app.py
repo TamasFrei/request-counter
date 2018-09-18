ae14ae0afe5962a3a005d42bda7f6214dff4bc14
Tamas Frei <frei.tamas555@gmail.com>
2018-09-18 11:04:22 +0200

    done

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
 
+@app.route('/')
+def main():
+    global counter
+    counter += 1
+    return render_template('index.html', counter=counter)
+
+
 @app.route('/request-counter')
 def request_counter():
-    return
+    global counter
+    counter += 1
+    return redirect('/')
 
 
+counter = 0
 if __name__ == '__main__':
     app.run(port=5000,
             debug=True)
